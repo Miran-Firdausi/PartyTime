2ee1d55178b5bcfc2a4c677ae9e3f23ea38e910c
Miran <miranfirdausi027@gmail.com>
2024-02-22 00:11:01 +0530

    Created folder structure for frontend

---
+++ Frontend/pages/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+export default function Index() {
+  return (
+    <Layout >
+        <h1>Index Page</h1>
+    </Layout>
+  );
+}
