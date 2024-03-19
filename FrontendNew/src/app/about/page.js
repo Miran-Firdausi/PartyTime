156bb03dd5b32f1932cfcee4cc745b22f356bcb9
battlemachine <onkar69483@gmail.com>
2024-03-19 18:39:15 +0530

    Added Cart Page

--- FrontendNew/src/app/about/page.js
+++ FrontendNew/src/app/about/page.js
@@ -6,7 +6,6 @@ import Navbar from '@/components/Navbar';
 const AboutPage = () => {
   return (
     <div>
-      <Navbar />
       <div className={styles.aboutPage}>
       <h1>About PartyTime</h1>
       <p>In the contemporary retail landscape, a pressing challenge emerges from the imminent expiration of perishable goods, resulting in economic losses for shopkeepers and missed opportunities for consumers seeking discounted products. Simultaneously, the event planning sector encounters obstacles in efficiently procuring large quantities of supplies. Addressing this dual challenge, our proposed solution introduces an innovative e-commerce platform that seamlessly connects surplus perishable items nearing expiration with the demand for bulk orders for parties and events. This platform not only mitigates economic losses for shopkeepers but also offers event planners and hosts a dedicated space to conveniently purchase a wide array of supplies, including discounted perishable goods, party-related items, and non-perishable snacks. By providing a comprehensive solution for both challenges, our platform aims to reduce food waste, optimize party planning processes, and create a mutually beneficial ecosystem for shopkeepers and consumers alike.</p>
